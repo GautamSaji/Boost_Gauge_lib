c19a576637ad32c22514e453e3c244f2af3bf35d
Gautam Saji <126568040+GautamSaji@users.noreply.github.com>
2023-06-27 13:46:28 +0530

    Update Boost_Gauge.h
    added menu code

--- Boost_Gauge.h
+++ Boost_Gauge.h
@@ -11,8 +11,29 @@
 
 #include <Boost_Gauge_Fonts.h>
 #include <SplashScreen.h>
+#include <Preferences.h>
+
+//preferences macros
+
+#define BOOST_LIMIT_FLASH "Boostfl"
+#define BUZZER_STATUS_FLASH "Buzzerfl"
 
 // All macros
+#define MAX_BOOST_RESET_PIN 13
+#define JOYSTICK_CENTER_PIN 26
+#define JOYSTICK_LEFT_PIN  32
+#define JOYSTICK_RIGHT_PIN 27
+#define JOYSTICK_UP_PIN    25
+#define JOYSTICK_DOWN_PIN  33
+
+#define BUZZER_PIN 14
+#define BUZZER_GAP_DURATION 200
+#define BUZZER_BEEP_DURATION 100
+
+#define LED_WARNING_PIN 5
+#define LED_INIT_PIN 17
+
+
 #define RECT_X 8       //bar starting point
 #define RECT_Y 96
 #define RECT_WIDTH  20   //bar width
@@ -36,6 +57,50 @@
 #define BACKLIGHT_CHANNEL    0
 #define BACKLIGHT_RESOLUTION 8
 #define LCD_BRIGHTNESS 40   //lcd brightness value
+
+//menu highlight options
+ #define HIGHLIGHT_BOOST_LIMIT 0
+ #define HIGHLIGHT_BOOST_LIMIT_WARNING 1
+ #define CHANGE_BOOST_LIMIT_WARNING 2
+ #define CHANGE_BOOST_LIMIT 3
+ #define SELECTED_BOOST_LIMIT 4
+ #define DISPLAY_MAIN 5
+ 
+ 
+ #define FLAG_RESET 0
+ #define FLAG_SET 1
+
+//
+
+void IRAM_ATTR Max_Boost_Reset();
+void IRAM_ATTR Joystick_mid_ISR();
+void IRAM_ATTR Joystick_up_ISR();
+void IRAM_ATTR Joystick_down_ISR();
+void IRAM_ATTR Joystick_left_ISR();
+void IRAM_ATTR Joystick_right_ISR();
+
+//Strcucture to hold menu button flags
+struct Menu_Button{
+  bool  IsMenuActive;
+  bool IsMenuSubActive;
+  volatile uint8_t Joystick_center_FLAG=FLAG_RESET;
+  volatile uint8_t Joystick_left_FLAG=FLAG_RESET;
+  volatile uint8_t Joystick_right_FLAG=FLAG_RESET;
+  volatile uint8_t Joystick_up_FLAG=FLAG_RESET;
+  volatile uint8_t Joystick_down_FLAG=FLAG_RESET;
+
+  Menu_Button(){
+   IsMenuActive=false;
+   IsMenuSubActive=false;
+   /*Joystick_center_FLAG=FLAG_RESET;
+   Joystick_left_FLAG=FLAG_RESET;
+   Joystick_right_FLAG=FLAG_RESET;
+   Joystick_up_FLAG=FLAG_RESET;
+   Joystick_down_FLAG=FLAG_RESET;*/
+  }
+
+};
+//
 
 //Structure to hold the position of each bar in the bar gauge
 struct Bar_pos_struct {
@@ -66,6 +131,7 @@ uint8_t boost_limit;
 
 float alpha,psi_val,psi_val_prev,max_value;
 double last_avg,curr_avg;
+volatile uint8_t Max_Boost_Reset_FLAG;
 
 Boost_calculation(){
 raw_data=0;
@@ -76,6 +142,7 @@ psi_val_prev=0;
 max_value=0;
 last_avg=0;
 curr_avg=0;
+Max_Boost_Reset_FLAG=0;
 }
 
 };
@@ -109,7 +176,7 @@ Boost_display(){
 
 //Main Boost Gauge obj
 class Boost_Gauge{
-private:
+public:
 //TFT Component
 Boost_display BoostDisplay;
 //
@@ -121,11 +188,40 @@ Adafruit_ADS1115 ads;
 //Boost parameters & display boost
 Boost_calculation Boost_params;
 //
-public:
+
+//Menu button
+Menu_Button MenuButton;
+//
+
+//flash memory obj
+Preferences preferences;
+
+//configurations
+bool isBuzzerActive;
+
+bool isMenuHbuzzerDisplayed;
+bool isMenuHlimitDsiplayed;
+bool isMenuSubLimitDisplayed;
+//
+
+//buzzer control params
+  uint32_t BuzzerBeepDuration;
+  uint32_t BuzzerGapDuration;
+  uint32_t buzzer_previous_time;
+  bool beep_on;
+//
 /////default constructor
    Boost_Gauge (){
-
+  
   ads=Adafruit_ADS1115();
+  isBuzzerActive=true;
+  isMenuHbuzzerDisplayed=false;
+  isMenuHlimitDsiplayed=false;
+  isMenuSubLimitDisplayed=false;
+  BuzzerBeepDuration=BUZZER_BEEP_DURATION;
+  BuzzerGapDuration=BUZZER_GAP_DURATION;
+  buzzer_previous_time=0;
+  bool beep_on=false;
    }
 
 
@@ -134,7 +230,9 @@ void DisplaySplashScreen(uint8_t seconds); //displays splash screen stored in im
 void DisplayMain(void); //displays the main boost gauge screen
 void Test(void); //displays a test procedure on the display
 void DisplayBoost(void); //calculates and displays the boost on the screen
-
+void ResetMaxBoost(void);//resets and diplays max boost
+void DisplayMenu(uint8_t MenuParam);//displays menu with highlighter option
+bool WaitForButtonPress(void);
 };
 
 
